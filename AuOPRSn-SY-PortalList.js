27529f31af0505d5a9dadcfc41f61b0c93807975
teddysnp <56281353+teddysnp@users.noreply.github.com>
2024-09-06 12:53:24 +0800

    Update AuOPRSn-SY-PortalList.js

--- AuOPRSn-SY-PortalList.js
+++ AuOPRSn-SY-PortalList.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AuOPRSn-SY-PortalList
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        http://*/*
@@ -9,8 +9,8 @@
 // @grant        none
 // ==/UserScript==
 
-var gpausePortal=["金字塔","乒乓球桌","星摩尔广场","小区运动场"];
-var gpausePortalString=["测试挪1","重复了!!!","测试挪3","测试挪4"];
+var gpausePortal=["金字塔","乒乓球桌","星摩尔广场","小区运动场"],"卫工河畔健身器材";
+var gpausePortalString=["测试挪1","重复了!!!","测试挪3","测试挪4","向下"];
 
 (function() {
     'use strict';
