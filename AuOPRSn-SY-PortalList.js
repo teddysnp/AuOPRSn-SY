7326742f0d2876ffb1d9d5fc1dd81e69df680f28
teddysnp <56281353+teddysnp@users.noreply.github.com>
2024-09-06 12:46:00 +0800

    Create AuOPRSn-SY-PortalList.js

---
+++ AuOPRSn-SY-PortalList.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         AuOPRSn-SY-PortalList
+// @namespace    http://tampermonkey.net/
+// @version      2024-09-06
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+var gpausePortal=["乒乓球桌"];
+var gpausePortalString=["重复"];
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
