6916374e93dbee5345726d8e2543f5ba16be0114
teddysnp <56281353+teddysnp@users.noreply.github.com>
2024-09-11 10:43:18 +0800

    Update AutOPRSn-SY-test.js

--- AutOPRSn-SY-test.js
+++ AutOPRSn-SY-test.js
@@ -18,18 +18,6 @@ window.editData;
 window.photoData;
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-var gpausePortal=["金字塔","乒乓球桌aaa","星摩尔广场","小区运动场"];
-var gpausePortalString=["测试挪1","重复了!!!","测试挪3","测试挪4"];
-const mission ={  //名称,位置,开始,类型,已审,时间
-  name: "",
-  location: "",
-  action: "",
-  type: "",
-  done: "",
-  dt: ""
-  };
-const missionlist=[["职工文体广场","北一路万达","true","编辑","","2024-09-09"],["摆烂的天使","北一路万达","true","新增","","2024-09-09"],["丛林里的梅花鹿","北一路万达","false","编辑","","2024-09-09"]];
-
 var chsaddr=null;
 var engaddr=null;
 var lastchsaddr=null;
