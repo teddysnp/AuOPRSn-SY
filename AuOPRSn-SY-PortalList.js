afcaf3959f4240223fc9757189811ad270b415c1
teddysnp <56281353+teddysnp@users.noreply.github.com>
2024-09-21 21:26:02 +0800

    Update AuOPRSn-SY-PortalList.js

--- AuOPRSn-SY-PortalList.js
+++ AuOPRSn-SY-PortalList.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AuOPRSn-SY-PortalList
 // @namespace    http://tampermonkey.net/
-// @version      1.4.0
+// @version      1.5
 // @description  try to take over the world!
 // @author       You
 // @match        http://*/*
