4aedf7ecdec550712186130acba947191a58b2fe
teddysnp <56281353+teddysnp@users.noreply.github.com>
2024-09-22 18:51:48 +0800

    Update AuOPRSn-SY-PortalList.js

--- AuOPRSn-SY-PortalList.js
+++ AuOPRSn-SY-PortalList.js
@@ -1,16 +1,79 @@
 // ==UserScript==
-// @name         AuOPRSn-SY-PortalList
+// @name         AuOPRSnPlus-ErrCheck-SY
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.0
 // @description  try to take over the world!
 // @author       You
-// @match        http://*/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @match        https://wayfarer.nianticlabs.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nianticlabs.com
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+let errNumber = 10;  //问题po，自动重试次数
 
-    // Your code here...
-})();
+setInterval(() => {
+  console.log("检测是否错误");
+  if(document.querySelector("app-review-error")) {
+    createNotify("错误", {
+      body: "需要重试！",
+      icon: "https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/source/stop.ico",
+      requireInteraction: false
+    });
+    let errbtn = document.querySelector('button[class="wf-button wf-button--primary"]');
+    console.log("errbtn",errbtn);
+    console.log("errNumber",errNumber);
+    if(errbtn & errNumber>=0)
+    {
+      errNumber--;
+      console.log("error clicked!");
+      errbtn.click();
+    } else {
+      createNotify("错误", {
+        body: "重试："+errNumber+"次无法成功，需要人式干预",
+        icon: "https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/source/stop.ico",
+        requireInteraction: true
+      });
+    }
+  }
+},10000);
+
+//用户消息
+function createNotify(title, options) {
+  var PERMISSON_GRANTED = "granted";
+  var PERMISSON_DENIED = "denied";
+  var PERMISSON_DEFAULT = "default";
+
+  // 如果用户已经允许，直接显示消息，如果不允许则提示用户授权
+  if (Notification.permission === PERMISSON_GRANTED) {
+    notify(title, options);
+  } else {
+    Notification.requestPermission(function (res) {
+      if (res === PERMISSON_GRANTED) {
+        notify(title, options);
+      }
+    });
+  }
+}
+//显示消息
+function notify($title, $options) {
+  var notification = new Notification($title, $options);
+  //    console.log(notification);
+  notification.onshow = function (event) {
+    //      console.log("show : ", event);
+  };
+  notification.onclose = function (event) {
+    //      console.log("close : ", event);
+  };
+  notification.onclick = function (event) {
+    //      console.log("click : ", event);
+    //      console.log("notify:title:"+title);
+    notification.close();
+    mywin.focus();
+    //      console.log("https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/images/"+title+".png");
+    //      checkImgExists("https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/images/"+title+".png").then(res =>{
+    //        mywin.open("https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/images/"+title+".png");
+    //      },err=>{console.log("Image not found!");});
+  };
+}
+
+
