1cc66ea299f374bbbde4ca617120f8dae0f5800a
teddysnp <56281353+teddysnp@users.noreply.github.com>
2024-09-23 06:22:53 +0800

    Update AuOPRSn-SY-PortalList.js

--- AuOPRSn-SY-PortalList.js
+++ AuOPRSn-SY-PortalList.js
@@ -9,86 +9,88 @@
 // @grant        none
 // ==/UserScript==
 
-let errNumber = 10;  //问题po，自动重试次数
-//let mywin=window;
+(function() {
+    let errNumber = 10;  //问题po，自动重试次数
+    let mywin = window;
 
-setInterval(() => {
-  console.log("检测是否错误");
-  if(document.querySelector("app-review-error")) {
-    let errbtn = document.querySelector('button[class="wf-button wf-button--primary"]');
-    console.log("errbtn",errbtn);
-    console.log("errNumber",errNumber);
-    if(!errbtn){
-      createNotify("错误", {
-        body: "重新加载",
-        icon: "https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/source/stop.ico",
-        requireInteraction: true
-      });
-      mywin.location.reload();
-      return;
-    }
-    if(errbtn)
-    {
-        if(errNumber>0){
-            createNotify("错误", {
-                body: "需要重试！",
-                icon: "https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/source/stop.ico",
-                requireInteraction: false
+    setInterval(() => {
+        console.log("检测是否错误");
+        if(document.querySelector("app-review-error")) {
+            let errbtn = document.querySelector('button[class="wf-button wf-button--primary"]');
+            console.log("errbtn",errbtn);
+            console.log("errNumber",errNumber);
+            if(!errbtn){
+                createNotify("错误", {
+                    body: "重新加载",
+                    icon: "https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/source/stop.ico",
+                    requireInteraction: true
+                });
+                mywin.location.reload();
+                return;
+            }
+            if(errbtn)
+            {
+                if(errNumber>0){
+                    createNotify("错误", {
+                        body: "需要重试！",
+                        icon: "https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/source/stop.ico",
+                        requireInteraction: false
+                    });
+                    errNumber--;
+                    console.log("error clicked!");
+                    errbtn.click();
+                }
+            }
+            if(errNumber==0) {
+                createNotify("错误", {
+                    body: "重试："+errNumber+"次无法成功，需要人工干预",
+                    icon: "https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/source/stop.ico",
+                    requireInteraction: true
+                });
+                //      mywin.location.reload();
+                clearInterval(this);
+            }
+        }
+    },10000);
+
+    //用户消息
+    function createNotify(title, options) {
+        var PERMISSON_GRANTED = "granted";
+        var PERMISSON_DENIED = "denied";
+        var PERMISSON_DEFAULT = "default";
+
+        // 如果用户已经允许，直接显示消息，如果不允许则提示用户授权
+        if (Notification.permission === PERMISSON_GRANTED) {
+            notify(title, options);
+        } else {
+            Notification.requestPermission(function (res) {
+                if (res === PERMISSON_GRANTED) {
+                    notify(title, options);
+                }
             });
-            errNumber--;
-            console.log("error clicked!");
-            errbtn.click();
         }
     }
-      if(errNumber==0) {
-      createNotify("错误", {
-        body: "重试："+errNumber+"次无法成功，需要人工干预",
-        icon: "https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/source/stop.ico",
-        requireInteraction: true
-      });
-//      mywin.location.reload();
-          clearInterval(this);
+    //显示消息
+    function notify($title, $options) {
+        var notification = new Notification($title, $options);
+        //    console.log(notification);
+        notification.onshow = function (event) {
+            //      console.log("show : ", event);
+        };
+        notification.onclose = function (event) {
+            //      console.log("close : ", event);
+        };
+        notification.onclick = function (event) {
+            //      console.log("click : ", event);
+            //      console.log("notify:title:"+title);
+            notification.close();
+            mywin.focus();
+            //      console.log("https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/images/"+title+".png");
+            //      checkImgExists("https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/images/"+title+".png").then(res =>{
+            //        mywin.open("https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/images/"+title+".png");
+            //      },err=>{console.log("Image not found!");});
+        };
     }
-  }
-},10000);
 
-//用户消息
-function createNotify(title, options) {
-  var PERMISSON_GRANTED = "granted";
-  var PERMISSON_DENIED = "denied";
-  var PERMISSON_DEFAULT = "default";
-
-  // 如果用户已经允许，直接显示消息，如果不允许则提示用户授权
-  if (Notification.permission === PERMISSON_GRANTED) {
-    notify(title, options);
-  } else {
-    Notification.requestPermission(function (res) {
-      if (res === PERMISSON_GRANTED) {
-        notify(title, options);
-      }
-    });
-  }
-}
-//显示消息
-function notify($title, $options) {
-  var notification = new Notification($title, $options);
-  //    console.log(notification);
-  notification.onshow = function (event) {
-    //      console.log("show : ", event);
-  };
-  notification.onclose = function (event) {
-    //      console.log("close : ", event);
-  };
-  notification.onclick = function (event) {
-    //      console.log("click : ", event);
-    //      console.log("notify:title:"+title);
-    notification.close();
-    mywin.focus();
-    //      console.log("https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/images/"+title+".png");
-    //      checkImgExists("https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/images/"+title+".png").then(res =>{
-    //        mywin.open("https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/images/"+title+".png");
-    //      },err=>{console.log("Image not found!");});
-  };
-}
-
+})();
 
