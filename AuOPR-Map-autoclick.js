5ad3fd7fc8a89ddf61a595fd50e6140525c620f1
teddysnp <56281353+teddysnp@users.noreply.github.com>
2024-09-26 20:36:35 +0800

    Update AuOPR-Map-autoclick.js

--- AuOPR-Map-autoclick.js
+++ AuOPR-Map-autoclick.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         AuOPR-Map-autoclick
+// @name         AuOPR-Map-autoclick-test
 // @namespace    http://tampermonkey.net/
-// @version      1.0.3
+// @version      1.0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://wayfarer.nianticlabs.com/*
@@ -9,7 +9,7 @@
 // @grant        none
 // ==/UserScript==
 //挪po指南：
-// 1-9:左至右排列第几个  11:18:上至下排列第几个  10:最右一个  20:最下一个
+// 1-9:左至右排列第几个  11:19:上至下排列第几个  10:最右一个  20:最下一个
 (function() {
     let ioutput = "true";
     let editGYMPosition = [["丛林里的梅花鹿","false","10"],["职工文体广场","false","2"],["万达贾飞碟","false","11"]];
@@ -41,7 +41,6 @@
         console.log('Promise', e)});
 
     function injectTimer() {
-        ioutput= "true";
         awaitElement(() => document.querySelector('wf-logo'))
             .then((ref) => {
             try {
@@ -58,25 +57,17 @@
     }
 
     setInterval(() => {
-//                console.log("timer");
-        //   if ioutput
-        //      监听是否有挪po操作
-        //      if portaldata
-        //        if portaldata edit
-        //        发现挪po
-        //      if document.query
-
-        if(ioutput!="false"){
-            console.log("监听是否有挪po操作");
-            if(portalData){
-                if(portalData.type=="EDIT"){
-                    console.log("发现挪po申请");
-                    console.log("查找地图上的点");
-                    if(document.querySelector('agm-map div[role="button"]')){
+        if(document.querySelector('agm-map div[role="button"]')){
+            if(ioutput=="true"){
+                console.log("监听是否有挪po操作");
+                if(portalData){
+                    if(portalData.type=="EDIT"){
+                        console.log("发现挪po申请");
+                        console.log("查找地图上的点");
                         let ptbutton = document.querySelectorAll('agm-map div[role="button"]');
                         let ptstruct = getbtnStruct(ptbutton);
                         let ititle = findArrayTwo(editGYMPosition,portalData.title);
-                        //                        console.log("ititle",ititle);
+                                                console.log("ititle",ititle);
                         if(ititle>=0){
                             //                            console.log(editGYMPosition[ititle]);
                             //                            console.log(editGYMPosition[ititle][2]);
@@ -86,36 +77,35 @@
                             if( movepos >10) movepos = movepos-10;
                             console.log("GYMData",ptstruct);
                             console.log("resortGYM",resortdata);
-                            let resdata=null;
                             console.log(movepos);
-                            if(movepos==10) {resdata=resortdata[resortdata.length - 1];}
-                            if(movepos<=resortdata.length){
-                                resdata=resortdata[movepos-1];
+                            console.log(resortdata.length);
+                            if(movepos<=resortdata.length || movepos == 10){
+                                let resdata=null;
+                                if(movepos==10) {resdata=resortdata[resortdata.length - 1];} else {resdata=resortdata[movepos-1];}
+                                console.log("resdata",resdata);
+                                ptbutton.forEach((ptbtn)=>{
+                                    //left: 65px; top: -135px;
+                                    if(ptbtn.getAttribute('style').indexOf("left: "+resdata.left +"px; top: "+resdata.top)>=0){
+                                        console.log("选中",ptbtn);
+                                        ptbtn.click();
+                                        ioutput=false;
+                                        return;
+                                    }
+                                })
                             }
-                            console.log("resdata",resdata);
-                            ptbutton.forEach((ptbtn)=>{
-                                //left: 65px; top: -135px;
-                                if(ptbtn.getAttribute('style').indexOf("left: "+resdata.left +"px; top: "+resdata.top)>=0){
-                                    console.log("选中",ptbtn);
-                                    ptbtn.click();
-                                    ioutput="false";
-                                    return;
-                                }
-                            })
                         } else {
                             //没找到，xjb选一个
                             ptbutton[0].click();
-                            console.log("xjb选一个");
-                            ioutput="false";
+                            ioutput=false;
                         }
                     }
                 }
                 else {
+                    console.log(portalData);
                     ioutput="false";
                 }
             }
         }
-
     },1000);
 
 
