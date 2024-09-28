ba71985f5d8b898cdc1120e624e66e4741724dc8
teddysnp <56281353+teddysnp@users.noreply.github.com>
2024-09-29 06:59:25 +0800

    Create AutOPRSn-SY-Options3.js

---
+++ AutOPRSn-SY-Options3.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         AutOPRSn-SY-Options3.js
+// @namespace    http://tampermonkey.net/
+// @version      2024-09-28
+// @version      1.0
+// @description  审po专用
+// @author       snpsl
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
