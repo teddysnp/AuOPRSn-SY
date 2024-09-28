7fd3f470b8cdd3657cfbac38fb77962cc2d47ad7
teddysnp <56281353+teddysnp@users.noreply.github.com>
2024-09-29 07:04:30 +0800

    Create AuOPRSn-SY-Load.js

---
+++ AuOPRSn-SY-Load.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         AuOPRSn-SY-Load
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  审po专用
+// @author       snpsl
+// @match        https://wayfarer.nianticlabs.com/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require      https://unpkg.com/ajax-hook@2.0.3/dist/ajaxhook.min.js
+// @require      https://github.com/teddysnp/AuOPRSn-SY/raw/main/AutOPRSn-SY.js
+// @require      https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/AuOPRSn-SY-PortalList.js
+// @require      https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/refs/heads/main/AuOPR-Map-autoclick.js
+// @require      https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/AutOPRSn-SY-Options1.js
+// @require      https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/AutOPRSn-SY-Options2.js
+// @require      https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/AutOPRSn-SY-Options3.js
+// @updateURL    https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/AuOPRSn-SY-Load.js
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
