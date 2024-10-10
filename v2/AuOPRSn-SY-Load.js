f5b72224854669e0dcd1cfb77d33c5d664ba015c
teddysnp <56281353+teddysnp@users.noreply.github.com>
2024-10-10 21:11:54 +0800

    Update AuOPRSn-SY-Load.js

--- v2/AuOPRSn-SY-Load.js
+++ v2/AuOPRSn-SY-Load.js
@@ -7,7 +7,7 @@
 // @match        https://wayfarer.nianticlabs.com/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require      https://unpkg.com/ajax-hook@2.0.3/dist/ajaxhook.min.js
-// @require      https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/v2/AuOPRSn-SY-Main.js
+// @require      https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/refs/heads/main/v2/AuOPRSn-SY-Main.js
 // @require      https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/v2/AuOPRSn-SY-PortalList.js
 // @require      https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/v2/AuOPRSn-SY-MapAutoclick.js
 // @require      https://raw.githubusercontent.com/teddysnp/AuOPRSn-SY/main/v2/AuOPRSn-SY-ErrCheck.js
