a5902910a45c4e7a12ac0427767d771043fac495
teddysnp <56281353+teddysnp@users.noreply.github.com>
2026-02-02 07:09:31 +0800

    Update AuOPRSn-SY-Options1.js

--- v2/AuOPRSn-SY-Options1.js
+++ v2/AuOPRSn-SY-Options1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AuOPRSn-SY-Options1
 // @namespace    AuOPR
-// @version      1.3
+// @version      1.4
 // @description  适应20260129,wayfarer新版：功能为显示任务和已经审po
 // @author       SnpSL
 // @match        https://wayfarer.nianticlabs.com/*
@@ -1259,7 +1259,38 @@
         style.innerHTML = css;
         document.querySelector('head').appendChild(style);
     })()
-
+    //格式化日期函数
+    function formatDate(date, fmt)
+    {
+        date = date == undefined ? new Date() : date;
+        date = typeof date == 'number' ? new Date(date) : date;
+        fmt = fmt || 'yyyy-MM-dd HH:mm:ss';
+        var obj =
+            {
+                'y': date.getFullYear(), // 年份，注意必须用getFullYear
+                'M': date.getMonth() + 1, // 月份，注意是从0-11
+                'd': date.getDate(), // 日期
+                'q': Math.floor((date.getMonth() + 3) / 3), // 季度
+                'w': date.getDay(), // 星期，注意是0-6
+                'H': date.getHours(), // 24小时制
+                'h': date.getHours() % 12 == 0 ? 12 : date.getHours() % 12, // 12小时制
+                'm': date.getMinutes(), // 分钟
+                's': date.getSeconds(), // 秒
+                'S': date.getMilliseconds() // 毫秒
+            };
+        var week = ['天', '一', '二', '三', '四', '五', '六'];
+        for(var i in obj)
+        {
+            fmt = fmt.replace(new RegExp(i+'+', 'g'), function(m)
+                              {
+                var val = obj[i] + '';
+                if(i == 'w') return (m.length > 2 ? '星期' : '周') + week[val];
+                for(var j = 0, len = val.length; j < m.length - len; j++) val = '0' + val;
+                return m.length == 1 ? val : val.substring(val.length - m.length);
+            });
+        }
+        return fmt;
+    }
 
     // 配置项：可根据需求修改
     const TARGET_NODE_ID = 'idmission'; // 目标节点ID
